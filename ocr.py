e09f26a1fef73905defd934004f904347e99d518
Hansika1406 <hansika1121@gmail.com>
2026-07-05 14:04:37 +0530

    Fixed OCR configuration for Windows and PDF support

--- ocr.py
+++ ocr.py
@@ -4,25 +4,32 @@ import numpy as np
 import re
 from pdf2image import convert_from_path
 
-pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
+# Windows Tesseract Path
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 
 def preprocess_image(img):
     img = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     gray = cv2.fastNlMeansDenoising(gray, None, 30, 7, 21)
 
-    kernel = np.array([[0, -1, 0],
-                       [-1, 5, -1],
-                       [0, -1, 0]])
+    kernel = np.array([
+        [0, -1, 0],
+        [-1, 5, -1],
+        [0, -1, 0]
+    ])
+
     sharp = cv2.filter2D(gray, -1, kernel)
 
     thresh = cv2.adaptiveThreshold(
-        sharp, 255,
+        sharp,
+        255,
         cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
         cv2.THRESH_BINARY,
-        11, 2
+        11,
+        2
     )
 
     return thresh
@@ -38,21 +45,36 @@ def extract_text(file_path):
     try:
         full_text = ""
 
+        # -------- PDF --------
         if file_path.lower().endswith(".pdf"):
+
             try:
-                images = convert_from_path(file_path)
-            except:
-                return "OCR ERROR: Invalid or corrupted PDF"
+                images = convert_from_path(
+    file_path,
+    poppler_path=r"C:\poppler\poppler-26.02.0\Library\bin"
+)
+            except Exception as e:
+                return f"OCR ERROR: {e}"
 
             for img in images:
                 img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+
                 processed = preprocess_image(img)
+
                 text = pytesseract.image_to_string(processed)
+
                 full_text += text + " "
 
+        # -------- IMAGE --------
         else:
+
             img = cv2.imread(file_path)
+
+            if img is None:
+                return "OCR ERROR: Unable to read image"
+
             processed = preprocess_image(img)
+
             full_text = pytesseract.image_to_string(processed)
 
         if not full_text.strip():
